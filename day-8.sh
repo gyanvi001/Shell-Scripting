225fde9e27d7c333e02c8f7bdbc0dd07af929829
gyanvi001 <gyanvipandey01@gmail.com>
2025-04-05 23:48:32 +0530

    API req & Data Fetch using Curl

---
+++ day-8.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#Function to display usage
+show_usage() {
+ echo "Usage: $0 <city_name"
+ echo "Example: $0 Delhi"
+}
+
+#Check if city is provided
+CITY="$1"
+if [ -z "$CITY" ]; then 
+echo "Error: City name is not present"
+show_usage
+exit 1
+fi
+
+# Displaying a loading message
+echo "Fetching weather details for $CITY.. "
+
+#Check if city is provided
+WEATHER =$(curl -s "https://wttr.in/${CITY}?format=3")
+
+#Check if the response is valid
+if [[ "$WEATHER" == *"Unknown location"* ]]; then 
+  echo "Error: Unable to fetch weather details for '$CITY'. Please check the city name"
+  exit 1
+fi
+
+#Display the weather details
+echo "======================="
+echo "   WEATHER REPORT       "
+echo "========================"
+echo "$WEATHER"
+echo "========================"
+
