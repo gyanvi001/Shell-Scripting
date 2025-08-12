69c2a0da699cc586e643ba0c6dd5743f61717e66
Gyanvi Pandey <155666897+gyanvi001@users.noreply.github.com>
2025-08-12 22:48:15 +0530

    Update.sh

--- day15.sh
+++ day15.sh
@@ -6,5 +6,7 @@ echo " Find and delete the log files older than seven days $log_dir"
 find "$log_dir" -type f -name ".*log" -mtime +"$Days_old" -exec rm -f {} \;
 echo "Deleted the log files"
 
+# Process Monitoring
+# Write a script to display the top 10 memory-consuming processes.
 
 
