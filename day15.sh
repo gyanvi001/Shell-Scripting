0eb717b81092fa3362e90ab42c7e212d499ca139
Gyanvi Pandey <155666897+gyanvi001@users.noreply.github.com>
2025-08-12 22:43:24 +0530

    Update.sh

--- day15.sh
+++ day15.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
-
+#!bin/bash
 log_dir = "/var/log"
-Days_old = 7 
+Days_old = 7
 
-echo "Find and delete files older than $Days_old in $log_dir ...."
-
-find "$log_dir" -type f -name "*.log" -mtime +"$Days_old" -exec rm -f {} \;
-
+echo " Find and delete the log files older than seven days $log_dir"
+find "$log_dir" -type f -name ".*log" -mtime +"$Days_old" -exec rm -f {} \;
 echo "Deleted the log files"
+
+
+
